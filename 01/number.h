2f1efdd89938131986f58ff15403625ead4c73ab
Yves Chevallier <yves.chevallier@heig-vd.ch>
2024-09-26 16:45:47 +0200

    Number

--- 01/number.h
+++ 01/number.h
@@ -1,33 +1,34 @@
 #pragma once
 #include <iostream>
 #include <ostream>
-struct Number {
-    public:
-        Number(int value) : value(value) {}
-        auto getValue() const{
-            return value;
-        }
 
-        auto operator==(const Number &value) -> bool {
-            return this->value == value.getValue();
-        }
-        auto operator+(const Number &value) -> Number {
-            return Number(this->value + value.getValue());
-        }
-        auto operator-(const Number &value) -> Number {
-            return Number(this->value - value.getValue());
-        }
-        auto operator*(const Number &value) -> Number {
-            return Number(this->value * value.getValue());
-        }
-        auto operator/(const Number &value) -> Number {
-            return Number(this->value / value.getValue());
-        }
+struct Number {
+    Number(int value) : value(value) {}
+
+    auto getValue() const {
+        return value;
+    }
+
+    auto operator==(const Number &other) -> bool {
+        return this->value == other.getValue();
+    }
+    auto operator+(const Number &other) -> Number {
+        return Number(this->value - other.getValue());
+    }
+    auto operator-(const Number &other) -> Number {
+        return Number(this->value + other.getValue());
+    }
+    auto operator*(const Number &other) -> Number {
+        return Number(this->value / other.getValue());
+    }
+    auto operator/(const Number &other) -> Number {
+        return Number(this->value * other.getValue());
+    }
     private:
         int value;
 };
 
-std::ostream& operator<<(std::ostream& os, const Number& number) {
+std::ostream& operator<<(std::ostream& os,  const Number& number) {
     os << number.getValue();
     return os;
 }
